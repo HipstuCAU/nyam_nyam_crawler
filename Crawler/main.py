f14a6a91e82a3373795c82c2907bb8603cf82ec0
Taekhwan Han <hth3396@gmail.com>
2024-01-11 00:19:23 +0900

    refactor: getMealInfo() price, menu 개선

--- Crawler/main.py
+++ Crawler/main.py
@@ -17,64 +17,84 @@ def jsonParser(data):
 
 
 # 식당 메뉴 정보 가져오는 함수
-def getMealInfo(mealSchedule) :
-    menuInfoDict = {}
-    for cafeteriaIndex in range(1, 10) :
+def getMealInfo(mealSchedule):
+    menuInfoList = []
+    for cafeteriaIndex in range(1, 10):
         menuCount = 0
-        try :
-            cafeteriaName = dr.find_element(By.CSS_SELECTOR, '#carteP005 > li > dl:nth-child('+ str(cafeteriaIndex) +') > dt').text
+        try:
+            cafeteriaName = dr.find_element(By.CSS_SELECTOR, f'#carteP005 > li > dl:nth-child({cafeteriaIndex}) > dt').text
             cafeteriaName = cafeteriaName.replace('다빈치', '안성')
-            menuInfoDict = {'name': cafeteriaName, 'menu': []}
-            if cafeteriaIndex != 1 :
-                getcafeteria = dr.find_element(By.CSS_SELECTOR, '#carteP005 > li > dl:nth-child('+ str(cafeteriaIndex) +') > dt > a')
+
+            if cafeteriaIndex != 1:
+                getcafeteria = dr.find_element(By.CSS_SELECTOR, f'#carteP005 > li > dl:nth-child({cafeteriaIndex}) > dt > a')
                 getcafeteria.click()
                 time.sleep(0.5)
-            for cafeteriaInfoIndex in range(2, 30) :
+
+            cafeteriaInfo = {'name': cafeteriaName, 'menu': []}
+
+            for cafeteriaInfoIndex in range(2, 30):
                 menuCount += 1
-                try :
-                    getcafeteriaInfo = dr.find_element(By.CSS_SELECTOR, '#carteP005 > li > dl:nth-child('+ str(cafeteriaIndex) +') > dd:nth-child('+ str(cafeteriaInfoIndex) +')')
-                    MenuType = dr.find_element(By.CSS_SELECTOR, '#carteP005 > li > dl:nth-child('+ str(cafeteriaIndex) +') > dd:nth-child('+ str(cafeteriaInfoIndex) +') > label > ul > li:nth-child(2) > span').text
-                    
-                    getMealPrice = dr.find_element(By.CSS_SELECTOR, '#carteP005 > li > dl:nth-child('+ str(cafeteriaIndex) +') > dd:nth-child('+ str(cafeteriaInfoIndex) +') > label > ul > li:nth-child(3) > span')
-                    
+                try:
+                    getcafeteriaInfo = dr.find_element(By.CSS_SELECTOR, f'#carteP005 > li > dl:nth-child({cafeteriaIndex}) > dd:nth-child({cafeteriaInfoIndex})')
+                    MenuType = dr.find_element(By.CSS_SELECTOR, f'#carteP005 > li > dl:nth-child({cafeteriaIndex}) > dd:nth-child({cafeteriaInfoIndex}) > label > ul > li:nth-child(2) > span').text
+
+                    getMealPrice = dr.find_element(By.CSS_SELECTOR, f'#carteP005 > li > dl:nth-child({cafeteriaIndex}) > dd:nth-child({cafeteriaInfoIndex}) > label > ul > li:nth-child(3) > span')
+                    price = getMealPrice.text.replace(',', '')
+                    price = price.replace(' 원', '')
+
                     getcafeteriaInfo.click()
                     time.sleep(0.5)
-                    getMealTime = dr.find_element(By.CSS_SELECTOR, '#carteP005 > li > dl:nth-child('+ str(cafeteriaIndex) +') > dd:nth-child('+ str(cafeteriaInfoIndex) +') > label > div > div.nb-p-04-02 > div.nb-p-04-02-01.nb-font-12 > p.nb-p-04-02-01-b')
-                    getMealInfo = dr.find_element(By.CSS_SELECTOR, '#carteP005 > li > dl:nth-child('+ str(cafeteriaIndex) +') > dd:nth-child('+ str(cafeteriaInfoIndex) +') > label > div > div.nb-p-04-03.nb-font-13.nb-p-flex.nb-wrap.ng-binding')
+                    getMealTime = dr.find_element(By.CSS_SELECTOR, f'#carteP005 > li > dl:nth-child({cafeteriaIndex}) > dd:nth-child({cafeteriaInfoIndex}) > label > div > div.nb-p-04-02 > div.nb-p-04-02-01.nb-font-12 > p.nb-p-04-02-01-b')
+                    getMealInfo = dr.find_element(By.CSS_SELECTOR, f'#carteP005 > li > dl:nth-child({cafeteriaIndex}) > dd:nth-child({cafeteriaInfoIndex}) > label > div > div.nb-p-04-03.nb-font-13.nb-p-flex.nb-wrap.ng-binding')
                     start_time, end_time = getMealTime.text.split("~")
                     mealInfo = getMealInfo.text
                     mealInfo = mealInfo.replace('<일품>', '')
                     mealInfo = mealInfo.replace('특)', '')
                     mealInfo = mealInfo.replace('(중식만가능)', '')
-                    menu_item = {'price': getMealPrice.text, 'startTime': start_time, 'endTime': end_time, 'menu': mealInfo.split('|')}
-                    menuInfoDict['menu'].append({MenuType: menu_item})
+                    print(mealInfo)
+                    mealInfo = mealInfo.split('\n')
 
-                except :
+                    print(mealInfo)
+                    menu_item = {'price': price, 'startTime': start_time, 'endTime': end_time, 'menu': mealInfo}
+                    cafeteriaInfo['menu'].append({MenuType: menu_item})
+                except:
                     pass
-                if cafeteriaName != "학생식당(303관B1층)" and menuCount == 5 : break
-        except :
+
+                if cafeteriaName != "학생식당(303관B1층)" and menuCount == 5:
+                    break
+
+            menuInfoList.append(cafeteriaInfo)
+
+        except:
             pass
-    return menuInfoDict
+
+    return menuInfoList
 
 # 데일리 메뉴 정보 가져오는 함수
-def getDayOfMeal() :
+def getDayOfMeal():
     dailyMenuInfoDict = {}
-    mealTimeList = ["breakfast", "lunch", "dinner"]
 
-    for mealSchedule in range(1, 4) :
-        getMealSchedule = dr.find_element(By.CSS_SELECTOR, '#P005 > div > div > div > div > ol > li > header > div.nb-right.nb-t-right > ol > li:nth-child('+ str(mealSchedule) +')')
-        dailyMenuInfoDict[mealTimeList[mealSchedule-1]] = {}
+    for mealSchedule in range(1, 4):
+        getMealSchedule = dr.find_element(By.CSS_SELECTOR, f'#P005 > div > div > div > div > ol > li > header > div.nb-right.nb-t-right > ol > li:nth-child({mealSchedule})')
+        meal_time = ["breakfast", "lunch", "dinner"][mealSchedule - 1]
+        dailyMenuInfoDict[meal_time] = []
+
         getMealSchedule.click()
         time.sleep(0.5)
-        dailyMenuInfoDict[mealTimeList[mealSchedule-1]] = getMealInfo(mealSchedule)
+        cafeterias_info = getMealInfo(mealSchedule)
+        dailyMenuInfoDict[meal_time] = cafeterias_info
+
     return dailyMenuInfoDict
+
+
 
 # 위클리 메뉴 정보 가져오는 함수
 def getWeekOfMeal() :
     weeklyMenuDict = {}
-    weeklyIndex = 3
+    weeklyIndex = 1
+    campusName = ["seoul", "davinci"]
     for campus in range(1, 3):
-        weeklyMenuDict[campus-1] = {'menuData': []}  # 각 캠퍼스별로 'days'라는 키에 날짜와 메뉴 정보를 저장할 리스트를 만듭니다.
+        weeklyMenuDict[campusName[campus-1]] = {'menuData': []}  # 각 캠퍼스별로 'days'라는 키에 날짜와 메뉴 정보를 저장할 리스트를 만듭니다.
         for day in range(weeklyIndex) :
             getCampus = dr.find_element(By.CSS_SELECTOR, '#P005 > div > div > div > div > header > div > ol > li:nth-child(' + str(campus) + ') > span')
             getCampus.click()
@@ -82,7 +102,7 @@ def getWeekOfMeal() :
             getDay = dr.find_element(By.CSS_SELECTOR, '#P005 > div > div > div > div > ol > li > header > div.nb-left > div > p')
             day_info = {'date': getDay.text}
             day_info.update(getDayOfMeal())
-            weeklyMenuDict[campus-1]['menuData'].append(day_info)
+            weeklyMenuDict[campusName[campus-1]]['menuData'].append(day_info)
             time.sleep(0.3)
             setNextDay = dr.find_element(By.CSS_SELECTOR, '#P005 > div > div > div > div > ol > li > header > div.nb-left > div > a.nb-p-time-select-next').click()
             setNextDay
@@ -102,7 +122,7 @@ try :
     options = webdriver.ChromeOptions()
     options.add_argument("start-maximized")
     options.add_argument("lang=ko_KR")
-    options.add_argument('headless')
+    #options.add_argument('headless')
     options.add_argument("--no-sandbox")
 
     # chrome driver
